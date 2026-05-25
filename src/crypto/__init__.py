5fa38ab6a5d882700025d2298d524cd4b78b3936
Scura <asimsovereign@gmail.com>
2026-05-25 19:38:23 -0400

    Update exported functions in crypto package

--- src/crypto/__init__.py
+++ src/crypto/__init__.py
@@ -1,4 +1,4 @@
 # Crypto package for Ed25519 signing/verification
-from .signer import sign_intent, verify_receipt, generate_keypair
+from .signer import sign_intent, verify_receipt_signature, generate_keypair
 
-__all__ = ["sign_intent", "verify_receipt", "generate_keypair"]
+__all__ = ["sign_intent", "verify_receipt_signature", "generate_keypair"]
